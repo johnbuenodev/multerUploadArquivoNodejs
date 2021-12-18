e97d64a452d3a3d676fe0fd872e2717473dff903
johnbuenodev <johnbuenodev@gmail.com>
2021-12-18 01:02:27 -0300

    projeto nodejs upload arquivos com multer

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const multer = require('multer');
+const bodyparser = require('body-parser');
+const req = require('express/lib/request');
+
+const app = express();
+
+//função de middleware
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.get('/api/information', (req,res) => {
+    res.json({message:'Hello World novamente!'});
+});
+
+port = 3000;
+
+///Listando porta do server/rodar
+value = `Server Running in ${port}`;
+app.listen(port,'127.0.0.1',()=>{
+  console.log(value);
+});
+
+
+//npm start server.js
